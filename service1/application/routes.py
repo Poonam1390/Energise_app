ec732d6972cfad1a3a0f36fbdffdceb5c65576e3
Poonam Kumar <poonamkumar@Poonams-MacBook-Air.local>
2020-05-03 23:49:32 +0100

    DB working with the app with option to insert and also with preloaded DB data

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -15,7 +15,22 @@ def home():
 def routine():
     task=requests.get('http://service2:5001/task/name')
     times=requests.post('http://service3:5002/times',data=task.text)
-    routine=requests.post('http://service4:5003/routine/json',json={'task':task.text,'times':times.text})
+    allTheData = Play.query.all()
+    exerciseList = []
+    singList = []
+    danceList = []
+    
+    for allData in allTheData:
+        exerciseList.append(allData.exercise)
+        singList.append(allData.sing)
+        danceList.append(allData.dance)
+    if task.text == 'exercise':
+        routine=requests.post('http://service4:5003/routine/json',json={'taskData':exerciseList})
+    elif task.text == 'sing':
+        routine=requests.post('http://service4:5003/routine/json',json={'taskData':singList})
+    elif task.text == 'dance':
+        routine=requests.post('http://service4:5003/routine/json',json={'taskData':danceList})
+    
     return render_template('routine.html',title='energise',task=task.text,times=times.text,routine=str(routine.text))
 
 
